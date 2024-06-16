d3a8ec88985c24824b12da6cb2781dd0282f4fda
Bruno Martins <bgcngm@gmail.com>
2024-06-16 15:14:57 +0100

    vayre: Inherit from QTI and common Xiaomi FCMs
    Change-Id: I8d4d54bba1b3dff27c11b0034852697b818ee609
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- VayreConfig.mk
+++ VayreConfig.mk
@@ -93,6 +93,9 @@ LOC_HIDL_VERSION := 3.0
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
