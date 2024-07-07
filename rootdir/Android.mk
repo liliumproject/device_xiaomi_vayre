bd2e913bc90cf2daba11f50a602059074be1231d
HeroBuxx <me@herobuxx.me>
2024-07-07 16:55:07 +0000

    vayre: rootdir: Commonize fstab
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,32 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+ifneq ($(filter lavender,$(TARGET_DEVICE)),)
+LOCAL_SRC_FILES    := etc/fstab.qcom.lavender
+else
+LOCAL_SRC_FILES    := etc/fstab.qcom
+endif
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+ifneq ($(filter lavender,$(TARGET_DEVICE)),)
+LOCAL_SRC_FILES    := etc/fstab.qcom.lavender
+else
+LOCAL_SRC_FILES    := etc/fstab.qcom
+endif
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.zram
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
