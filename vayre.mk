438874a19e159a3ee233d55ed2cb74ed168dbc59
HeroBuxx <me@herobuxx.me>
2024-07-07 18:30:59 +0000

    vayre: Commonize and update blobs
    Co-authored-by: Alcatraz323 <alcatraz32323@gmail.com>
    Co-authored-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- vayre.mk
+++ vayre.mk
@@ -296,7 +296,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0.vendor
+    android.hardware.keymaster@4.1.vendor
 
 # Lights
 PRODUCT_PACKAGES += \
